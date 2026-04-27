f1a229ccaf802426a0d5c10aa9e9f1e8a2f1c89e
Thiago Esteves <thiago.esteves@erlang-solutions.com>
2026-04-27 14:19:28 -0300

    Initial commit from default grisp project

---
+++ lib/nerves_grisp.ex
@@ -0,0 +1,18 @@
+defmodule NervesGrisp do
+  @moduledoc """
+  Documentation for `NervesGrisp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesGrisp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_grisp_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesGrispTest do
+  use ExUnit.Case
+  doctest NervesGrisp
+
+  test "greets the world" do
+    assert NervesGrisp.hello() == :world
+  end
+end
